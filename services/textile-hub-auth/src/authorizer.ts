1fed460db93577e33667945cafbc54267c8ea3d7
studna <vojtech.studenka@gmail.com>
2020-08-12 13:41:26 +0200

    Authorizer lambda

---
+++ services/textile-hub-auth/src/authorizer.ts
@@ -0,0 +1,76 @@
+import {
+  CustomAuthorizerResult,
+  Callback,
+  Context,
+  CustomAuthorizerEvent,
+} from 'aws-lambda';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+interface AuthContext {
+  username: string;
+  pubkey: string;
+}
+
+const generatePolicy = function(
+  principalId: string,
+  effect: string,
+  resource: string,
+  context: AuthContext
+): CustomAuthorizerResult {
+  // Required output:
+  const authResponse: any = {
+    principalId,
+    context,
+  };
+
+  authResponse.principalId = principalId;
+
+  if (effect && resource) {
+    const policyDocument = {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    };
+
+    authResponse.policyDocument = policyDocument;
+  }
+
+  return authResponse;
+};
+
+const generateAllow = (
+  principalId: string,
+  resource: string,
+  context: AuthContext
+): CustomAuthorizerResult =>
+  generatePolicy(principalId, 'Allow', resource, context);
+
+// eslint-disable-next-line
+export const handler = async function(event: CustomAuthorizerEvent, _context: Context, callback: Callback<CustomAuthorizerResult>) {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+
+  // Retrieve request parameters from the Lambda function input:
+  const { headers } = event;
+
+  try {
+  const decoded = jwt.verify(headers.Authorization, JWT_SECRET);
+
+  callback(
+    null,
+    generateAllow('me', event.methodArn, { pubkey: decoded.pubkey, username: decoded.username })
+  );
+  } catch (e) {
+    callback('Unauthorized request');
+  }
+};
+
+// const generateDeny = function(principalId, resource) {
+//   return generatePolicy(principalId, 'Deny', resource);
+// };
