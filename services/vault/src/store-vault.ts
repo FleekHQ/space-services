6dfbbd52aaa559dac0a135afb2781e46a3ddd29c
Daniel Merrill <danielmerrill6@gmail.com>
2020-08-21 10:55:57 -0400

    Vault WIP

--- services/vault/src/store-vault.ts
+++ services/vault/src/store-vault.ts
@@ -1,21 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { IdentityModel, VaultModel } from '@packages/models';
+import { VaultModel, UnauthorizedError } from '@packages/models';
 import { processRequest } from '@packages/apitools';
-import crypto from 'crypto';
 import { StoreVaultRequest } from './types';
+import { computeVskHash } from './shared';
 
 if (!process?.env?.ENV) {
   throw new Error('ENV variable not set');
 }
 
-const saltSecret = process?.env?.SALT_SECRET;
-if (!saltSecret || saltSecret === '') {
-  throw new Error('SALT_SECRET variable not set');
-}
-
 const STAGE = process.env.ENV;
-
-const identityDb = new IdentityModel(STAGE);
 const vaultDb = new VaultModel(STAGE);
 
 export const storeVault = async (
@@ -24,19 +17,16 @@ export const storeVault = async (
   const result = await processRequest(
     async (): Promise<void> => {
       const request: StoreVaultRequest = JSON.parse(event.body);
-      const { uuid, vault, vsk } = request;
+      const { vault, vsk } = request;
+      const { uuid } = event.requestContext.authorizer;
 
-      // TODO: Verify JWT.uuid === uuid
+      if (!uuid || uuid === '') {
+        throw new UnauthorizedError('Authorization token is invalid.');
+      }
 
       // Calculate the key we store in the db to check if the user is providing the correct password
       // when retreiving the vault later on.
-      const vskHash = crypto.pbkdf2Sync(
-        vsk, // vsk should already be a secure password since it went through a kdf in the client side.
-        saltSecret + uuid, // In the event that keys got leaked, a brute force attacker would also need the saltSecret to be able to brute force
-        1_000_000, // Running 1.000.000 iterations takes about a second (at least in the year 2020)
-        64,
-        'sha512'
-      );
+      const vskHash = computeVskHash(vsk, uuid);
 
       // Store vskHash together with the vault
       await vaultDb.storeVault({
