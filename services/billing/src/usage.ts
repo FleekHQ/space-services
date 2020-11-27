ec6529692185acb124fd4bdf5ff13cd16f60ba99
studna <vojtech.studenka@gmail.com>
2020-11-27 15:00:32 +0100

    Mock endpoints for usage

---
+++ services/billing/src/usage.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyResult, APIGatewayProxyEventBase } from 'aws-lambda';
+// import createDbModel from '@packages/models/dist/billing/dbModel';
+
+if (!process?.env?.ENV) {
+  throw new Error('ENV variable not set');
+}
+
+// const STAGE = process.env.ENV;
+// const dbModel = createDbModel(STAGE);
+
+export interface AuthContext {
+  uuid: string;
+  pubkey: string;
+}
+
+// eslint-disable-next-line
+export const handler = async (
+  event: APIGatewayProxyEventBase<AuthContext>
+): Promise<APIGatewayProxyResult> => {
+  const { uuid } = event.requestContext.authorizer;
+
+  console.log(uuid);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      storage: Math.random() * 100000000,
+      bandwidth: Math.random() * 1000000,
+    }),
+  };
+};
